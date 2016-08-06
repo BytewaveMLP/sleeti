0f3e317bf57370f98df70ed61890da2950e28059
Eliot Partridge <shortcircuit369@gmail.com>
2016-08-06 11:35:34 -0500

    Basic user model

---
+++ app/Models/User.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Eeti\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+	protected $table = 'users'; // not exactly necessary, but why not
+
+	protected $fillable = [
+		'username',
+		'email',
+		'name',
+		'password',	
+	];
+}
