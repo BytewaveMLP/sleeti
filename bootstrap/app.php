797b214cfadb103972b71874ce1fc4dbc8355268
Eliot Partridge <shortcircuit369@gmail.com>
2016-08-06 14:12:05 -0500

    Message flashing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -40,6 +40,10 @@ $container['auth'] = function ($container) {
 	return new \Eeti\Auth\Auth;
 };
 
+$container['flash'] = function ($container) {
+	return new \Slim\Flash\Messages;
+};
+
 $container['view'] = function ($container) {
 	$view = new \Slim\Views\Twig(__DIR__ . '/../resources/views', [
 		'cache' => false,
@@ -54,6 +58,8 @@ $container['view'] = function ($container) {
 		'check' => $container->auth->check(),
 		'user'  => $container->auth->user(),
 	]);
+
+	$view->getEnvironment()->addGlobal('flash', $container->flash);
 
 	return $view;
 };
