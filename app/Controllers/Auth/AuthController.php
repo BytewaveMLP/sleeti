f55bba6513fb238bf83a0e7567580cbfb9c9f093
Eliot Partridge <shortcircuit369@gmail.com>
2016-08-11 13:27:05 -0500

    whoops, spacing

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -65,7 +65,7 @@ class AuthController extends Controller
 
 		$userPerms->user()->associate($user); // TODO: see if this is redundant
 
-		$this->container->flash->addMessage('success', '<b>Success!</b> Welcome to' . $this->container->settings['site']['title'] . '!');
+		$this->container->flash->addMessage('success', '<b>Success!</b> Welcome to ' . $this->container->settings['site']['title'] . '!');
 
 		if ($user->id == 1) { // if this is the only user, give them admin
 			$this->container->flash->addMessage('info', 'New administrative account created!');
